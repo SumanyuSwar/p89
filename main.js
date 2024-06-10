b44c4d9424af8ded0acf586c2c3e504672052104
Sudip Swar <155371916+SumanyuSwar@users.noreply.github.com>
2024-06-10 21:43:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+function addUser(){
+    localStorage.setItem("User 1", document.getElementById("user_in_1").value)
+    localStorage.setItem("User 2", document.getElementById("user_in_2").value)
+}
